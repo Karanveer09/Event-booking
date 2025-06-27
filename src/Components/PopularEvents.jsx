d6a6076636d006a40f9f091098cb51f90bad2367
Your Name <you@example.com>
2025-06-27 19:09:26 +0530

    Replaced all files with new content

--- src/Components/PopularEvents.jsx
+++ src/Components/PopularEvents.jsx
@@ -2,6 +2,17 @@ import concert from '/public/concert.webp';
 import theatre from '/public/theatre.jpg';
 import cricket from '/public/cricket.jpg';
 import bbq from '/public/bbq.jpeg';
+// import bassi from '/public/bassi.jpeg';
+import stndup from '/public/stndup.jpeg';
+import urbanfestival from '/public/urbanfestival.jpeg';
+import DeadpoolWolverine from '/public/DeadpoolWolverine.webp';
+import legendsleague from '/public/legendsleague.jpeg';
+// import zakirkhan from '/public/zakirkhan.jpg';
+import standup from '/public/standup.jpeg';
+import football from '/public/football.jpeg';
+import parmishVerma from '/public/parmishVerma.jpg';
+import pathaan from '/public/pathaan.avif';
+import badminton from '/public/badminton.avif';
 import EventCard from './Eventcard';
 
  function PopularEvents() {
@@ -33,7 +44,85 @@ import EventCard from './Eventcard';
       date: "Tue, July 10",
       location: "Sector 17 Plaza",
       price: 849
-    }
+    },
+    {
+    image: concert,
+    title: "Prem Dhillon Live",
+    date: "Sat, July 20",
+    location: "Elante Courtyard, Chandigarh",
+    price: 999
+  },
+     {
+    image: stndup,
+    title: "Anubhav Singh Bassi Stand-up",
+    date: "Sun, July 28",
+    location: "Panjab University Auditorium",
+    price: 899
+  },
+{
+    image: theatre,
+    title: "Courtroom Drama - Hindi Play",
+    date: "Sat, July 19",
+    location: "Alliance Française, Sector 36",
+    price: 599
+  },
+
+    {
+    image: urbanfestival,
+    title: "Punjabi Folk Festival",
+    date: "Wed, July 30",
+    location: "Sector 17 Plaza",
+    price: 599
+  },
+  {
+  image: DeadpoolWolverine,
+  title: "Deadpool & Wolverine",
+  date: "Fri, July 26",
+  location: "Cinepolis, Bestech Mall Mohali",
+  price: 399
+},
+{
+  image: legendsleague,
+  title: "Legends League - India XI vs World XI",
+  date: "Sat, August 3",
+  location: "Sector 16 Cricket Stadium, Chandigarh",
+  price: 799
+},
+   {
+    image: standup,
+    title: "Zakir Khan - Live in Chandigarh",
+    date: "Mon, July 22",
+    location: "Tagore Theatre, Chandigarh",
+    price: 799
+  },
+  {
+  image: football,
+  title: "Chandigarh Warriors vs Punjab FC - Football League",
+  date: "Sun, July 21",
+  location: "Sector 42 Sports Complex, Chandigarh",
+  price: 499
+},  
+  ,{
+  image: parmishVerma,
+  title: "Parmish Verma Live in Concert",
+  date: "Sat, August 10",
+  location: "Indradhanush Auditorium, Panchkula",
+  price: 1199
+},
+    {
+    image: pathaan,
+    title: "Pathaan Returns",
+    date: "Fri, July 12",
+    location: "PVR Cinemas, Elante Mall",
+    price: 299
+  },
+    {
+  image: badminton,
+  title: "Chandigarh Open Badminton Tournament",
+  date: "Tue, July 23",
+  location: "St. John’s Indoor Stadium, Sector 26",
+  price: 299
+},
   ];
 
   return (
